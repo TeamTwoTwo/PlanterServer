6045e96f4a02e5b345c77f7af55a77656d6eb140
suyeoniii <ssy4230@naver.com>
2022-10-30 21:16:32 +0900

    :zap: 상태코드 200, 500으로 구성

--- src/main/kotlin/com/twotwo/planter/util/BaseResponseCode.kt
+++ src/main/kotlin/com/twotwo/planter/util/BaseResponseCode.kt
@@ -5,11 +5,11 @@ import org.springframework.http.HttpStatus
 enum class BaseResponseCode(isSuccess: Boolean, code: Int, status: HttpStatus, message: String) {
     SUCCESS(true, 1000, HttpStatus.OK,"성공"),
 
-    DUPLICATE_EMAIL(false, 3001, HttpStatus.CONFLICT, "중복된 이메일입니다"),
-    INVALID_CODE(false, 3002, HttpStatus.BAD_REQUEST, "인증번호가 일치하지 않습니다"),
-    DUPLICATE_PHONE(false, 3003, HttpStatus.CONFLICT, "중복된 휴대폰번호입니다"),
-    USER_NOT_FOUND(false, 3004, HttpStatus.NOT_FOUND, "이메일, 비밀번호가 일치하지 않습니다"),
-    CREDENTIAL_INVALID(false, 3005, HttpStatus.BAD_REQUEST, "이메일, 비밀번호가 일치하지 않습니다")
+    DUPLICATE_EMAIL(false, 3001, HttpStatus.OK, "중복된 이메일입니다"),
+    INVALID_CODE(false, 3002, HttpStatus.OK, "인증번호가 일치하지 않습니다"),
+    DUPLICATE_PHONE(false, 3003, HttpStatus.OK, "중복된 휴대폰번호입니다"),
+    USER_NOT_FOUND(false, 3004, HttpStatus.OK, "이메일, 비밀번호가 일치하지 않습니다"),
+    CREDENTIAL_INVALID(false, 3005, HttpStatus.OK, "이메일, 비밀번호가 일치하지 않습니다")
 ;
 
     val isSuccess: Boolean = isSuccess
